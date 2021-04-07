67f4399e94dbeb908d9859a77e8d3d714a9a16d6
Leonardo Tolosa <tolosa@gmail.com>
2021-04-07 19:16:58 -0300

    Add configuration support

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ const MyPlugin = require('./src')
 module.exports = function (api) {
   api.cache(true)
 
-  const plugins = [MyPlugin]
+  const plugins = [
+    [MyPlugin, { answer: 42 }]
+  ]
 
   return { plugins }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+const defaultOptions = {
+  identifier: 'answerToTheUltimateQuestionOfLifeTheUniverseAndEverything',
+  answer: 42
+}
+
 function myPlugin({ types: t }) {
   return {
     visitor: {
-      Identifier(path) {
+      Identifier(path, state) {
+        const options = { ...defaultOptions, ...state.opts }
         const { node } = path
-        if(node.name === 'answerToTheUltimateQuestionOfLifeTheUniverseAndEverything')
-          node.name = '42'
+        if(node.name === options.identifier) {
+          node.name = String(options.answer)
+        }
       }
     }
   }
